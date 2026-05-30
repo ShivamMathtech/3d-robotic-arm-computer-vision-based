961e704e5b16c075f6a98a30288fbfa1df67d8b2
ShivamMathtech <drshivam49@gmail.com>
2026-05-30 20:11:27 +0530

    ready with resial communication

---
+++ Python/serial_controller.py
@@ -0,0 +1,39 @@
+import cv2
+import mediapipe as mp
+
+class HandTracker:
+
+    def __init__(self):
+
+        self.mpHands = mp.solutions.hands
+
+        self.hands = self.mpHands.Hands(
+            max_num_hands=1,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7
+        )
+
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def get_landmarks(self, frame):
+
+        rgb = cv2.cvtColor(
+            frame,
+            cv2.COLOR_BGR2RGB
+        )
+
+        results = self.hands.process(rgb)
+
+        points = []
+
+        if results.multi_hand_landmarks:
+
+            hand = results.multi_hand_landmarks[0]
+
+            for lm in hand.landmark:
+
+                points.append(
+                    (lm.x, lm.y, lm.z)
+                )
+
+        return points
